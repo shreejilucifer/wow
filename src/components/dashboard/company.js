c529e103b8ec378f90237adadc956494d41a1f2a
Shreeji Pedhadiya <shreejilucifer@gmail.com>
2020-03-30 22:19:32 +0530

    Size of XY Axis

--- src/components/dashboard/company.js
+++ src/components/dashboard/company.js
@@ -12,43 +12,43 @@ import {
 
 const data = [
   {
-    name: 'Page A',
+    name: '12:00',
     uv: 4000,
     pv: 2400,
     amt: 2400
   },
   {
-    name: 'Page B',
+    name: '12:00',
     uv: 3000,
     pv: 1398,
     amt: 2210
   },
   {
-    name: 'Page C',
+    name: '12:00',
     uv: 2000,
     pv: 9800,
     amt: 2290
   },
   {
-    name: 'Page D',
+    name: '12:00',
     uv: 2780,
     pv: 3908,
     amt: 2000
   },
   {
-    name: 'Page E',
+    name: '12:00',
     uv: 1890,
     pv: 4800,
     amt: 2181
   },
   {
-    name: 'Page F',
+    name: '12:00',
     uv: 2390,
     pv: 3800,
     amt: 2500
   },
   {
-    name: 'Page G',
+    name: '12:00',
     uv: 3490,
     pv: 4300,
     amt: 2100
@@ -119,8 +119,8 @@ const Company = ({ company, theme }) => {
                   }}
                 >
                   <CartesianGrid strokeDasharray='3 3' />
-                  <XAxis dataKey='name' />
-                  <YAxis />
+                  <XAxis dataKey='name' tick={{ fontSize: '9px' }} />
+                  <YAxis tick={{ fontSize: '9px' }} />
                   <Tooltip />
                   <Line type='monotone' dataKey='pv' stroke='#6FB353' />
                 </LineChart>
