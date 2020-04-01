b38fd96b98bbfa922dc0d76bb0c95ab8128185d9
Shreeji Pedhadiya <shreejilucifer@gmail.com>
2020-04-02 02:57:26 +0530

    Theme Persists

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ThemeContext } from '../src/utils/theme';
+import Cookies from 'js-cookie';
+
 import '../src/styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  const [theme, setTheme] = useState(true);
+  const [theme, setActualTheme] = useState(true);
+
+  useEffect(() => {
+    if (Cookies.get('theme') === 'false') setActualTheme(false);
+  }, []);
 
   return (
     <div>
-      <ThemeContext.Provider value={{ theme, setTheme }}>
+      <ThemeContext.Provider
+        value={{
+          theme,
+          setTheme: theme => {
+            setActualTheme(theme);
+            Cookies.set('theme', theme);
+          }
+        }}
+      >
         <Component {...pageProps} />
       </ThemeContext.Provider>
     </div>
