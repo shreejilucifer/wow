04bae357a15b74943a055c084cab88373877ef25
Shreeji Pedhadiya <shreejilucifer@gmail.com>
2020-10-07 00:31:08 +0530

    searching in transactions

--- src/components/dashboard/transactions.tsx
+++ src/components/dashboard/transactions.tsx
@@ -1,9 +1,9 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { ThemeContext } from '../../utils/theme';
 import light from '../../styles/light/transactions.module.css';
 import dark from '../../styles/dark/transactions.module.css';
 import { useState } from 'react';
-import { useTransactionsQuery } from '../../generated/graphql';
+import { Transaction, useTransactionsQuery } from '../../generated/graphql';
 
 interface transactionItemProps {
   type: string;
@@ -37,7 +37,11 @@ const TransactionItem: React.FC<transactionItemProps> = ({
   );
 };
 
-const SearchBar = () => {
+interface searchbarProps {
+  onSearch: (str: string) => void;
+}
+
+const SearchBar: React.FC<searchbarProps> = ({ onSearch }) => {
   const { theme } = useContext(ThemeContext);
   const styles = theme ? light : dark;
   const [searchVisible, setSearchVisible] = useState(false);
@@ -46,7 +50,7 @@ const SearchBar = () => {
     <div className={styles.searchBarContainer}>
       {searchVisible ? (
         <div className={styles.searchbar}>
-          <input type='text' />
+          <input type='text' onChange={(e) => onSearch(e.target.value)} />
           <img
             alt='search'
             onClick={() => setSearchVisible(!searchVisible)}
@@ -67,33 +71,63 @@ const SearchBar = () => {
 const Transactions = () => {
   const { theme } = useContext(ThemeContext);
   const styles = theme ? light : dark;
-  const { data } = useTransactionsQuery();
+  const { data, refetch, loading } = useTransactionsQuery();
+
+  useEffect(() => {
+    refetch();
+  }, [data?.transactions]);
+
+  const [search, setSearch] = useState('');
+
+  const searchFilter = (transactions: any) => {
+    return transactions.filter((t: Transaction) =>
+      t.company.name.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>Transactions</div>
-      <SearchBar />
-      <div className={styles.header}>
-        <div>Type</div>
-        <div>ID</div>
-        <div>Company</div>
-        <div>Price</div>
-        <div>Quantity</div>
-        <div>Timestamp</div>
-        <div>Status</div>
-      </div>
-      <div className={styles.transactions}>
-        {data?.transactions.map((transaction) => (
-          <TransactionItem
-            type={transaction.type}
-            key={transaction.id}
-            companyName={transaction.company.name}
-            id={transaction.id.toString(16)}
-            price={transaction.shareAmount}
-            quantity={transaction.noOfShares}
-            time={`8:00`}
-          />
-        ))}
-      </div>
+      <SearchBar onSearch={(search) => setSearch(search)} />
+      {loading ? (
+        <div
+          style={{
+            height: '63vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <h3>Loading...</h3>
+        </div>
+      ) : (
+        <>
+          <div className={styles.header}>
+            <div>Type</div>
+            <div>ID</div>
+            <div>Company</div>
+            <div>Price</div>
+            <div>Quantity</div>
+            <div>Timestamp</div>
+            <div>Status</div>
+          </div>
+          <div className={styles.transactions}>
+            {searchFilter(data?.transactions).map(
+              (transaction: Transaction) => (
+                <TransactionItem
+                  type={transaction.type}
+                  key={transaction.id}
+                  companyName={transaction.company.name}
+                  id={transaction.id.toString(16)}
+                  price={transaction.shareAmount}
+                  quantity={transaction.noOfShares}
+                  time={`8:00`}
+                />
+              )
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
