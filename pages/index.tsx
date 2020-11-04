96bf919a0ebf0d7a6c2e3b844dafbafa7318e1d0
Shreeji Pedhadiya <shreejilucifer@gmail.com>
2020-11-04 21:01:30 +0530

    Login Mutation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import { Box, Button, Flex, Heading } from '@chakra-ui/core';
+import { Alert, Box, Button, Flex, Heading } from '@chakra-ui/core';
 import { Form, Formik } from 'formik';
+import { useRouter } from 'next/dist/client/router';
 import * as React from 'react';
 import { InputField } from '../components/InputField';
+import { useLoginAdminMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { withApollo } from '../utils/withApollo';
 
 interface IIndexPageProps {}
 
 const IndexPage: React.FunctionComponent<IIndexPageProps> = () => {
+	const router = useRouter();
+	const [login, { error }] = useLoginAdminMutation();
 	return (
 		<div>
 			<Flex
@@ -20,23 +26,38 @@ const IndexPage: React.FunctionComponent<IIndexPageProps> = () => {
 			<Flex height={500} justifyContent="center" alignItems="center">
 				<Box p={10} width={500} shadow="md">
 					<Formik
-						initialValues={{ username: '', password: '' }}
-						onSubmit={() => {
-							console.log('Submit');
+						initialValues={{ email: '', password: '' }}
+						onSubmit={async (values, { setErrors }) => {
+							const response = await login({
+								variables: values,
+							});
+							if (response.data?.loginAdmin.errors) {
+								setErrors(toErrorMap(response.data.loginAdmin.errors));
+							} else if (response.data?.loginAdmin.admin) {
+								if (typeof router.query.next === 'string') {
+									router.push(router.query.next);
+								} else {
+									router.push('/statistics');
+								}
+							}
 						}}
 					>
 						{({ isSubmitting }) => (
 							<Form>
 								<InputField
-									name="username"
-									label="Username"
+									disabled={isSubmitting}
+									name="email"
+									label="Email"
 									placeholder="johndoe"
+									type="text"
 								/>
 								<Box mt={4}>
 									<InputField
+										disabled={isSubmitting}
 										name="password"
 										label="Password"
 										placeholder="••••••••"
+										type="password"
 									/>
 								</Box>
 								<Flex mt={4} alignItems="center" justifyContent="space-between">
@@ -48,6 +69,7 @@ const IndexPage: React.FunctionComponent<IIndexPageProps> = () => {
 										Login
 									</Button>
 								</Flex>
+								{error && <Alert>{error.message}</Alert>}
 							</Form>
 						)}
 					</Formik>
@@ -57,4 +79,4 @@ const IndexPage: React.FunctionComponent<IIndexPageProps> = () => {
 	);
 };
 
-export default IndexPage;
+export default withApollo({ ssr: false })(IndexPage);
