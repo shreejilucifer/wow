51e6fc2562e0d5410075df31b9abf81b304952c4
Shreeji Pedhadiya <shreejilucifer@gmail.com>
2020-11-05 01:58:34 +0530

    secure pages

--- pages/broker.tsx
+++ pages/broker.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import Layout from '../components/Layout';
+import { useIsAuth } from '../utils/useIsAuth';
 import { withApollo } from '../utils/withApollo';
 
 interface IBrokerPageProps {}
 
 const BrokerPage: React.FunctionComponent<IBrokerPageProps> = () => {
+	useIsAuth();
 	return <Layout>Broker Page</Layout>;
 };
 
--- pages/company.tsx
+++ pages/company.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import AddCompany from '../components/AddCompany';
 import Layout from '../components/Layout';
 import ListCompany from '../components/ListCompany';
+import { useIsAuth } from '../utils/useIsAuth';
 import { withApollo } from '../utils/withApollo';
 
 interface ICompanyPageProps {}
 
 const CompanyPage: React.FunctionComponent<ICompanyPageProps> = () => {
+	useIsAuth();
 	return (
 		<Layout>
 			<AddCompany />
